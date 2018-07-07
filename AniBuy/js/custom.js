94c660e27a551cc45ea264611a361a95ca7c15d2
54x1 <fivefour.xone@gmail.com>
2018-07-07 13:39:07 +1000

    body

--- AniBuy/js/custom.js
+++ AniBuy/js/custom.js
@@ -7,16 +7,16 @@ $(document).ready(function () {
     var oncedone3 = false;
 
     $(this, '.grid-container').scroll(function () {
-        if ($(this, '.grid-container').scrollTop() > 2700 && oncedone1 == false) {
+        if ($(this, 'body').scrollTop() > 2800 && oncedone1 == false) {
                 $(".breakpoint-1").fadeIn(2500).css({ "display": "grid"});
                 $("#home-page-best-anime, .home-page-best-anime, #home-page-season-anime, .home-page-season-anime").hide();
                 oncedone1 = true;
             }
-            if ($(this, '.grid-container').scrollTop() > 4200 && oncedone2 == false) {
+            if ($(this, 'body').scrollTop() > 4200 && oncedone2 == false) {
                 $("#home-page-best-anime, .home-page-best-anime").fadeIn(2500).css({ "display": "grid"});
                 oncedone2 = true;
             }
-            if ($(this, '.grid-container').scrollTop() > 5280 && oncedone3 == false) {
+            if ($(this, 'body').scrollTop() > 5280 && oncedone3 == false) {
                 $("#home-page-season-anime, .home-page-season-anime").fadeIn(2500).css({ "display": "grid"});
                 oncedone3 = true;
             }
