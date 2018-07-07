77f70ba6ef521b9cb0a99b237b1890475be1c964
54x1 <fivefour.xone@gmail.com>
2018-07-07 12:03:40 +1000

    anibuy

---
+++ AniBuy/js/custom.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function () {
+    $('body').fadeIn(1000);
+
+    var oncedone1 = false;
+    var oncedone2 = false;
+    var oncedone3 = false;
+
+        $(window).scroll(function () {
+            if ($(window).scrollTop() > 2800 && oncedone1 == false) {
+                $(".breakpoint-1").fadeIn(2500);
+                $(".breakpoint-1").css({ "display": "grid" });
+                $("#home-page-best-anime, .home-page-best-anime, #home-page-season-anime, .home-page-season-anime").hide();
+                oncedone1 = true;
+            }
+            if ($(window).scrollTop() > 4200 && oncedone2 == false) {
+                oncedone1 = true;
+                $("#home-page-best-anime, .home-page-best-anime").fadeIn(2500);
+                $("#home-page-best-anime, .home-page-best-anime").css({ "display": "grid" });
+                oncedone2 = true;
+            }
+            if ($(window).scrollTop() > 5285 && oncedone3 == false) {
+                $("#home-page-season-anime, .home-page-season-anime").fadeIn(2500);
+                $("#home-page-season-anime, .home-page-season-anime").css({ "display": "grid", });
+                oncedone3 = true;
+            }
+            
+            
+        });
+        
+
+       
+});
