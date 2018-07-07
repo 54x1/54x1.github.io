dd9fa3da7831e66604d67565e86512330c00158b
54x1 <fivefour.xone@gmail.com>
2018-07-07 15:34:57 +1000

    scroll5

--- AniBuy/js/custom.js
+++ AniBuy/js/custom.js
@@ -7,17 +7,18 @@ $(document).ready(function () {
     var oncedone3 = false;
 
     $(window).scroll(function () {
-        var height = $('.grid-container').offset().top;
-        if (height > 2800 && oncedone1 == false) {
+        var height = $('.grid-container');
+        
+        if (height.scrollTop() > 2800 && oncedone1 == false) {
                 $(".breakpoint-1").fadeIn(2500).css({ "display": "grid"});
                 $("#home-page-best-anime, .home-page-best-anime, #home-page-season-anime, .home-page-season-anime").hide();
                 oncedone1 = true;
             }
-        if (height > 4200 && oncedone2 == false) {
+        if (height.scrollTop() > 4200 && oncedone2 == false) {
                 $("#home-page-best-anime, .home-page-best-anime").fadeIn(2500).css({ "display": "grid"});
                 oncedone2 = true;
             }
-        if (height > 5280 && oncedone3 == false) {
+        if (height.scrollTop(); > 5280 && oncedone3 == false) {
                 $("#home-page-season-anime, .home-page-season-anime").fadeIn(2500).css({ "display": "grid"});
                 oncedone3 = true;
             }
