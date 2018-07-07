99fdc6c27b12fd2e596e667c0dc4cf3b95d7ff2b
54x1 <fivefour.xone@gmail.com>
2018-07-07 13:30:34 +1000

    fix2

--- AniBuy/js/custom.js
+++ AniBuy/js/custom.js
@@ -7,19 +7,18 @@ $(document).ready(function () {
     var oncedone3 = false;
 
     $(this, '.grid-container').scroll(function () {
-        if ($(this, document, '.grid-container').scrollTop() > 2800 && oncedone1 == false) {
+        if ($(this, '.grid-container').scrollTop() > 2800 && oncedone1 == false) {
                 $(".breakpoint-1").fadeIn(2500);
                 $(".breakpoint-1").css({ "display": "grid" });
                 $("#home-page-best-anime, .home-page-best-anime, #home-page-season-anime, .home-page-season-anime").hide();
                 oncedone1 = true;
             }
-            if ($(this, document, '.grid-container').scrollTop() > 4200 && oncedone2 == false) {
-                oncedone1 = true;
+            if ($(this, '.grid-container').scrollTop() > 4200 && oncedone2 == false) {
                 $("#home-page-best-anime, .home-page-best-anime").fadeIn(2500);
                 $("#home-page-best-anime, .home-page-best-anime").css({ "display": "grid" });
                 oncedone2 = true;
             }
-            if ($(this, document, '.grid-container').scrollTop() > 5285 && oncedone3 == false) {
+            if ($(this, '.grid-container').scrollTop() > 5285 && oncedone3 == false) {
                 $("#home-page-season-anime, .home-page-season-anime").fadeIn(2500);
                 $("#home-page-season-anime, .home-page-season-anime").css({ "display": "grid"});
                 oncedone3 = true;
