6bc9c490c0821c34810d8b82a0ea86acb1888bac
54x1 <fivefour.xone@gmail.com>
2018-07-08 14:32:01 +1000

    fixed var

--- AniBuy/js/custom.js
+++ AniBuy/js/custom.js
@@ -8,20 +8,20 @@ $(document).ready(function () {
     var oncedone1v1 = false;
     var oncedone2v1 = false;
     var oncedone3v1 = false;
-    var oncedone4 = false;
+    var oncedone4v1 = false;
 
 
     $(document).ready(function(){
         $(window).scroll(function () {
             var bottom = $(window).scrollTop() + $(window).height() > $(document).height() - 100;
-            if ($(window).scrollTop() + $(window).height() > $(document).height() - 100 && oncedone1v1 == false && $(window).width() < 768) {
+            if ($(window).scrollTop() + $(window).height() > $(document).height() - 1 && oncedone1v1 == false && $(window).width() < 768) {
                 $(".breakpoint-1").fadeIn(2500).css({ "display": "grid" });
                 $("#home-page-best-anime, .home-page-best-anime, #home-page-season-anime, .home-page-season-anime, #home-page-genre-anime, #home-page-action-anime, .home-page-action-anime").hide();
-                if ($(window).scrollTop() + $(window).height() > $(document).height() - 100 && oncedone2v1 == false) {
+                if ($(window).scrollTop() + $(window).height() > $(document).height() - 1 && oncedone2v1 == false) {
                     $("#home-page-best-anime, .home-page-best-anime").fadeIn(2500).css({ "display": "grid" });
                     if ($(window).scrollTop() + $(window).height() > $(document).height() - 100 && oncedone3v1 == false) {
                         $("#home-page-season-anime, .home-page-season-anime").fadeIn(2500).css({ "display": "grid" });                        
-                        if ($(window).scrollTop() + $(window).height() > $(document).height() - 100 && oncedone4v1 == false) {
+                        if ($(window).scrollTop() + $(window).height() > $(document).height() - 1 && oncedone4v1 == false) {
                             $("#home-page-genre-anime, #home-page-action-anime, .home-page-action-anime").fadeIn(2500).css({ "display": "grid" });
                             oncedone1v1 = true;
                             oncedone2v1 = true;
