c86b5a8ad1e839e333a028b2c5a98997f6aa48ff
54x1 <fivefour.xone@gmail.com>
2018-07-08 13:11:47 +1000

    scroll+width effect

--- AniBuy/js/custom.js
+++ AniBuy/js/custom.js
@@ -5,44 +5,48 @@ $(document).ready(function () {
     var oncedone1 = false;
     var oncedone2 = false;
     var oncedone3 = false;
+    var oncedone1v1 = false;
+    var oncedone2v1 = false;
+    var oncedone3v1 = false;
     var oncedone4 = false;
 
 
-
-    $(window).scroll(function () {
-    var bottom = $(window).scrollTop() + $(window).height() > $(document).height() - 100;
-    if ($(window).scrollTop() + $(window).height() > $(document).height() - 100 && oncedone1 == false && $(window).width() < 768) {
-            $(".breakpoint-1").fadeIn(2500).css({ "display": "grid" });
-            $("#home-page-best-anime, .home-page-best-anime, #home-page-season-anime, .home-page-season-anime").hide();
-            if ($(window).scrollTop() + $(window).height() > $(document).height() - 100 && oncedone2 == false) {
-                $("#home-page-best-anime, .home-page-best-anime").fadeIn(2500).css({ "display": "grid" });
-                if ($(window).scrollTop() + $(window).height() > $(document).height() - 100 && oncedone3 == false) {
-                    $("#home-page-season-anime, .home-page-season-anime").fadeIn(2500).css({ "display": "grid" });
+    $(document).ready(function(){
+        $(window).scroll(function () {
+            var bottom = $(window).scrollTop() + $(window).height() > $(document).height() - 100;
+            if ($(window).scrollTop() + $(window).height() > $(document).height() - 100 && oncedone1 == false && $(window).width() < 768) {
+                $(".breakpoint-1").fadeIn(2500).css({ "display": "grid" });
+                $("#home-page-best-anime, .home-page-best-anime, #home-page-season-anime, .home-page-season-anime").hide();
+                if ($(window).scrollTop() + $(window).height() > $(document).height() - 100 && oncedone2 == false) {
+                    $("#home-page-best-anime, .home-page-best-anime").fadeIn(2500).css({ "display": "grid" });
+                    if ($(window).scrollTop() + $(window).height() > $(document).height() - 100 && oncedone3 == false) {
+                        $("#home-page-season-anime, .home-page-season-anime").fadeIn(2500).css({ "display": "grid" });
+                        oncedone1v1 = true;
+                        oncedone2v1 = true;
+                        oncedone3v1 = true;
+                        /*if (bottom && oncedone4 == false) {
+    
+                        }*/
+                    }
+                }
+            }
+            if ($(window).scrollTop() + $(window).height() > $(document).height() - 100 && oncedone1 == false && $(window).width() > 768) {
+                $(".breakpoint-1").fadeIn(2500).css({ "display": "grid" });
+                $("#home-page-best-anime, .home-page-best-anime, #home-page-season-anime, .home-page-season-anime").hide();
+                if ($(window).scrollTop() + $(window).height() > $(document).height() - 100 && oncedone2 == false) {
+                    $("#home-page-best-anime, .home-page-best-anime, #home-page-season-anime, .home-page-season-anime").fadeIn(2500).css({ "display": "grid" });
                     oncedone1 = true;
                     oncedone2 = true;
                     oncedone3 = true;
                     /*if (bottom && oncedone4 == false) {
-
+        
                     }*/
+
                 }
             }
-        }
+        });
     });
     
-    if ($(window).scrollTop() + $(window).height() > $(document).height() - 1 && oncedone1 == false && $(window).width() > 768) {
-        $(".breakpoint-1").fadeIn(2500).css({ "display": "grid" });
-        $("#home-page-best-anime, .home-page-best-anime, #home-page-season-anime, .home-page-season-anime").hide();
-        if ($(window).scrollTop() + $(window).height() > $(document).height() - 100 && oncedone2 == false) {
-            $("#home-page-best-anime, .home-page-best-anime, #home-page-season-anime, .home-page-season-anime").fadeIn(2500).css({ "display": "grid" });
-            oncedone1 = true;
-            oncedone2 = true;
-            oncedone3 = true;
-            /*if (bottom && oncedone4 == false) {
-
-            }*/
-                
-        }
-    }
+    
 });
-       
-});
+    
