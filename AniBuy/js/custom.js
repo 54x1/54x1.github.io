2063e14089a68417262bb518b40c0cc8de4cb2ff
54x1 <fivefour.xone@gmail.com>
2018-07-13 23:02:17 +1000

    read and buy done

--- AniBuy/js/custom.js
+++ AniBuy/js/custom.js
@@ -2,18 +2,6 @@
 $(document).ready(function () {
     $('body').fadeIn(1000);
 
-    var oncedone1 = false;
-    var oncedone2 = false;
-    var oncedone1v1 = false;
-    var oncedone2v1 = false;
-    var oncedone3v1 = false;
-    var oncedone4v1 = false;
-    var oncedone1v2 = false;
-    var oncedone2v2 = false;
-    var oncedone3v2 = false;
-    var oncedone4v2 = false;
-
-    $(document).ready(function () {
         $(window).on('scroll resize', function (i) {
             $('.bpshow').each(function () {
                 if ($(window).scrollTop() + $(window).height() == $(document).height() && $(window).width() > 768) {
@@ -21,8 +9,4 @@ $(document).ready(function () {
                 }
             });
         });
-
-
- 
-            });
-        });
+    });
