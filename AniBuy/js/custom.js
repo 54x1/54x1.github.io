b5c5bd2bb37241effaa5b4fc9961d1bc74f881c1
54x1 <fivefour.xone@gmail.com>
2018-07-15 15:37:00 +1000

    fadein times

--- AniBuy/js/custom.js
+++ AniBuy/js/custom.js
@@ -4,8 +4,11 @@ $(document).ready(function () {
 
         $(window).on('scroll resize', function (i) {
             $('.bpshow').each(function () {
-                if ($(window).scrollTop() + $(window).height() == $(document).height()/* && $(window).width() > 768*/) {
-                    $(this).fadeIn(1500).css({ "display": "grid" });
+                if ($(window).scrollTop() + $(window).height() == $(document).height() && $(window).width() > 768) {
+                    $(this).fadeIn(2000).css({ "display": "grid" });
+                }
+                if ($(window).scrollTop() + $(window).height() == $(document).height() && $(window).width() < 768) {
+                    $(this).fadeIn(250).css({ "display": "grid" });
                 }
 
             });
