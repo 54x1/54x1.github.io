71dbe843bfa77f9132aa74a109507c889941ac1f
54x1 <fivefour.xone@gmail.com>
2018-07-22 19:08:20 +1000

    fixed the anime-pop bug + window height = nav(74)

--- AniBuy/js/custom.js
+++ AniBuy/js/custom.js
@@ -3,13 +3,30 @@ $(document).ready(function () {
     $(document).ready(onresize);
     $(window).ready(onresize);
 
+    //fix this bug on 784px
+    $(window).on('load resize', function (i) {
+        var vheight = $('.grid-row-3').height();
+        var vheight2 =$('.grid-row-4').height();
+        var gdr3 = $('.grid-row-3');
+        var gdr4 = $('.grid-row-4');
+        var gdr5 = $('.grid-row-5');
+        var winw = window.innerWidth;
+
+        if(winw < 768 | winw < 768 && vheight > vheight2){
+            gdr3.height(gdr4.height()+gdr5.height());
+        }
+       else if (winw>768){
+            gdr3.css({"height":"100%"});
+        }
+    });
+
 
     var my = $("#myCarousel, .carousel-inner, .carousel-control, div.item, div.item img");
-    if (my.height($(window).height() - 82) > 500 && $(window).width() > 768) {
-        my.height($(window).height() - 82);
+    if (my.height($(window).height() - 74) > 500 && $(window).width() > 768) {
+        my.height($(window).height() - 74);
     }
-    if (my.height($(window).height() - 82) < 500 && $(window).width() < 768) {
-        my.height($(window).height() - 82);
+    if (my.height($(window).height() - 74) < 500 && $(window).width() < 768) {
+        my.height($(window).height() - 74);
     }
 
     var nav = $('.nav-wrapper');
