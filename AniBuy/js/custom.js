94f29e305f3ea5d4c8e107bff0dba2608abc5df5
54x1 <fivefour.xone@gmail.com>
2018-07-23 18:08:54 +1000

    changed margin on read and buy and changed scroll position

--- AniBuy/js/custom.js
+++ AniBuy/js/custom.js
@@ -74,11 +74,8 @@ $(document).ready(function () {
     var nav = $('.nav-wrapper');
     var NavTp = $('.nav-wrapper').offset().top;
     $(document).scroll(function () {
-        var vscroll = $(this).scrollTop();
-        if ($(window).width() > 992) {
-            $('.slider-buy').css({ "transform": "translate(" + vscroll / 1.5 + "px, -" + vscroll / 20 + "px)" });
-            $('.slider-read').css({ "transform": "translate(-" + vscroll / 3 + "px, -" + vscroll / 5 + "px)" });
-        }
+ 
+
         var scroll = $(window).scrollTop();
         if (scroll <= NavTp) {  
             nav.css({ "position": "static"});
@@ -91,9 +88,17 @@ $(document).ready(function () {
     });
    
     $(window).on('scroll resize', function (i) {
-
+        var vscroll = $(this).scrollTop();
+        if ($(window).width() > 992) {
+            $('.slider-buy').css({ "transform": "translate(" + vscroll / 1.5 + "px, -" + vscroll / 20 + "px)" });
+            $('.slider-read').css({ "transform": "translate(-" + vscroll / 3 + "px, -" + vscroll / 5 + "px)" });
+        }
+        if ($(window).width() < 992) {
+            $('.slider-buy, .slider-read').css({ "transform": "translate(0,0)" });
+        }
         
         $('.bpshow').each(function () {
+            var vscroll = $(this).scrollTop();
             if ($(window).scrollTop() + $(window).height() >= $(document).height() - 100 && $(window).width() > 768) {
                 $(this).fadeIn(2500).css({ "display": "grid" });
             }
