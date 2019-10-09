fb066322bd3fd7e0728b1abe50d0c903b04bb11c
54x1 <five4x1@gmail.com>
2019-10-09 22:18:33 +1100

    update for mobile and some desktop

--- TaweDgame/js/mainjs.js
+++ TaweDgame/js/mainjs.js
@@ -3,7 +3,9 @@ $(document).ready(function () {
 
         var hscroll = $(this).scrollTop();
         var TopOfWin1 = $(window).scrollTop();
-        var TopOfObj2 = $('.main-rules').position().top;
+        var TopOfObj1 = $('.main-rules').position().top;
+        var BotOfWin1 = $(window).scrollTop() + $(window).outerHeight();
+        var BotOfObj1 = ($('.main-rules').position().top + $('.main-rules').outerHeight()) - 1;
 
         function transfuc1() {
             $('.logoT').css({
@@ -29,33 +31,20 @@ $(document).ready(function () {
             });
         }
 
-
         if ($(this).width() <= 2560) {
             console.log("2560");
-            if ($(window).width() > 1920) {
-                $('.logoT').css({
-                    "transform": "translate(-" + hscroll / 0.5 + "px,-" + hscroll / 0.8 + "px)"
-                });
-                $('.logoD').css({
-                    "transform": "translate(" + hscroll / 0.5 + "px, -" + hscroll / 0.8 + "px)"
-                });
-            }
             if ($(window).width() < 1920) {
-                $('.logoT, .logoD').css({
-                    "transform": "translate(0,0)"
-                });
-                $('.logoOR').css({
-                    "transform": "scale(0)"
-                });
-                $('.main-rules, .mobile-header, .main-home').css({
-                    "transform": "translateY(0)"
-                });
-                $('.arrows-1').css({
-                    "transform": "translate(0px, 0)"
-                });
+                transfuc2();
             }
             if ($(window).width() > 1920) {
-                if (TopOfWin1 > (TopOfObj2-50)) {
+                transfuc1();
+                if (BotOfWin1 > BotOfObj1) {
+
+                    $('.game-section').css({"display": "block"});
+                } else {
+                    $('.game-section').css({"display": "none"});
+                }
+                if (TopOfWin1 > (TopOfObj1 - 50)) {
                     $('.logoOR').css({
                         "transform": "scale(0)"
                     });
@@ -69,7 +58,6 @@ $(document).ready(function () {
                         "display": "none"
                     });
 
-
                 } else {
                     $('.main-header').css({
                         "display": "grid"
@@ -79,7 +67,7 @@ $(document).ready(function () {
                         "overflow-x": "hidden"
                     });
                     $('.logoOR').css({
-                        "transform": "scale(" + (hscroll / 140) + ")"
+                        "transform": "scale(" + (hscroll / 190) + ")"
                     });
                     $('.main-rules, .mobile-header, .main-home').css({
                         "transform": "translateY(-" + (hscroll / 0.85) + "px)"
@@ -93,35 +81,20 @@ $(document).ready(function () {
             }
         }
 
-
         if ($(this).width() <= 1920) {
             console.log("1920");
-            // if ($(window).width() > 1600) {
-            //     $('.logoT').css({
-            //         "transform": "translate(-" + hscroll / 0.5 + "px,-" + hscroll / 0.8 + "px)"
-            //     });
-            //     $('.logoD').css({
-            //         "transform": "translate(" + hscroll / 0.5 + "px, -" + hscroll / 0.8 + "px)"
-            //     });
-            // }
             if ($(window).width() < 1600) {
-transfuc2();
-                // $('.logoT, .logoD').css({
-                //     "transform": "translate(0,0)"
-                // });
-                // $('.logoOR').css({
-                //     "transform": "scale(0)"
-                // });
-                // $('.main-rules, .mobile-header, .main-home').css({
-                //     "transform": "translateY(0)"
-                // });
-                // $('.arrows-1').css({
-                //     "transform": "translate(0px, 0)"
-                // });
+                transfuc2();
             }
             if ($(window).width() > 1600) {
                 transfuc1();
-                if (TopOfWin1 > (TopOfObj2 + 50)) {
+                if (BotOfWin1 > BotOfObj1) {
+
+                    $('.game-section, .arrows-2').css({"display": "block"});
+                } else {
+                    $('.game-section, .arrows-2').css({"display": "none"});
+                }
+                if (TopOfWin1 > (TopOfObj1 - 50)) {
                     $('.main-header').css({
                         "display": "none"
                     });
@@ -135,9 +108,6 @@ transfuc2();
                     $('.gofullscreen').css({
                         "overflow-x": "visible"
                     });
-                    // $('.main-rules').css({
-                    //     "transform": "translate(0px, 0px)"
-                    // });
 
                 } else {
                     $('.main-header').css({
@@ -192,7 +162,7 @@ transfuc2();
             }
             if ($(window).width() > 1440) {
 
-                if (TopOfWin1 > (TopOfObj2-50)) {
+                if (TopOfWin1 > (TopOfObj1 - 50)) {
                     $('.logoOR').css({
                         "transform": "scale(0)"
                     });
@@ -254,7 +224,7 @@ transfuc2();
                 });
             }
             if ($(window).width() > 1366) {
-                if (TopOfWin1 > (TopOfObj2-50)) {
+                if (TopOfWin1 > (TopOfObj1 - 50)) {
                     $('.logoOR').css({
                         "transform": "scale(0)"
                     });
@@ -316,7 +286,7 @@ transfuc2();
             }
             if ($(window).width() > 1200) {
 
-                if (TopOfWin1 > (TopOfObj2-50)) {
+                if (TopOfWin1 > (TopOfObj1 - 50)) {
                     $('.logoOR').css({
                         "transform": "scale(0)"
                     });
@@ -380,7 +350,7 @@ transfuc2();
             }
             if ($(window).width() > 1024) {
 
-                if (TopOfWin1 > (TopOfObj2-50)) {
+                if (TopOfWin1 > (TopOfObj1 - 50)) {
                     $('.logoOR').css({
                         "transform": "scale(0)"
                     });
@@ -442,7 +412,7 @@ transfuc2();
                 });
             }
             if ($(window).width() > 992) {
-                if (TopOfWin1 > (TopOfObj2-50)) {
+                if (TopOfWin1 > (TopOfObj1 - 50)) {
                     $('.logoOR').css({
                         "transform": "scale(0)"
                     });
@@ -479,27 +449,8 @@ transfuc2();
 
         if ($(this).width() <= 992) {
             console.log("992");
-            if ($(window).width() > 768) {
-                $('.logoT').css({
-                    "transform": "translate(-" + hscroll / 0.5 + "px,-" + hscroll / 0.8 + "px)"
-                });
-                $('.logoD').css({
-                    "transform": "translate(" + hscroll / 0.5 + "px, -" + hscroll / 0.8 + "px)"
-                });
-            }
             if ($(window).width() < 768) {
-                $('.logoT, .logoD').css({
-                    "transform": "translate(0,0)"
-                });
-                $('.logoOR').css({
-                    "transform": "scale(0)"
-                });
-                $('.main-rules, .mobile-header, .main-home').css({
-                    "transform": "translateY(0)"
-                });
-                $('.arrows-1').css({
-                    "transform": "translate(0px, 0)"
-                });
+                transfuc2();
             }
             if ($(window).width() > 768) {
                 if ((hscroll / 1.5) < 1) {
@@ -512,43 +463,89 @@ transfuc2();
                     $('.arrows-1').css({
                         "transform": "translate(0px, 0)"
                     });
+                }
+                transfuc1()
+                if (TopOfWin1 > (TopOfObj1 - 50)) {
+                    $('.logoOR').css({
+                        "transform": "scale(0)"
+                    });
+                    $('.logoT, .logoD').css({
+                        "transform": "translate(0px,-" + ((hscroll / 0.35)) + "px)"
+                    });
+                    $('.gofullscreen').css({
+                        "overflow-x": "visible"
+                    });
+                    $('.main-header').css({
+                        "display": "none"
+                    });
                 } else {
-                    if (TopOfWin1 > (TopOfObj2 - 50)) {
-                        $('.logoOR').css({
-                            "transform": "scale(0)"
-                        });
-                        $('.logoT, .logoD').css({
-                            "transform": "translate(0px,-" + ((hscroll / 0.35)) + "px)"
-                        });
-                        $('.gofullscreen').css({
-                            "overflow-x": "visible"
-                        });
-                        $('.main-header').css({
-                            "display": "none"
-                        });
-                    } else {
-                        $('.main-header').css({
-                            "display": "grid"
-                        });
+                    $('.main-header').css({
+                        "display": "grid"
+                    });
 
-                        $('.gofullscreen').css({
-                            "overflow-x": "hidden"
-                        });
+                    $('.gofullscreen').css({
+                        "overflow-x": "hidden"
+                    });
 
-                        $('.logoOR').css({
-                            "transform": "scale(" + (hscroll / 80) + ")"
-                        });
-                        $('.main-rules, .mobile-header, .main-home').css({
-                            "transform": "translateY(-" + (hscroll / 0.85) + "px)"
-                        });
-                        $('.arrows-1').css({
-                            "transform": "translate(0px,-" + ((hscroll / 0.35)) + "px)"
+                    $('.logoOR').css({
+                        "transform": "scale(" + (hscroll / 80) + ")"
+                    });
+                    $('.main-rules, .mobile-header, .main-home').css({
+                        "transform": "translateY(-" + (hscroll / 0.85) + "px)"
+                    });
+                    $('.arrows-1').css({
+                        "transform": "translate(0px,-" + ((hscroll / 0.35)) + "px)"
 
-                        });
-                    }
+                    });
                 }
             }
         }
+
+        if ($(this).width() <= 768) {
+            console.log("768");
+                transfuc2();
+                if (BotOfWin1 > BotOfObj1) {
+                    $('.game-section, .arrows-2').css({"display": "block"});
+                } else {
+                    $('.game-section, .arrows-2').css({"display": "none"});
+                }
+                if (TopOfWin1 > (TopOfObj1 - 50)) {
+                    $('.logoOR').css({
+                        "transform": "scale(0)"
+                    });
+                    $('.logoT, .logoD').css({
+                        "transform": "translate(0px,-0px)"
+                    });
+                    $('.gofullscreen, body, html').css({
+                        "overflow-x": "visible"
+                    });
+                    $('.main-header').css({
+                        "display": "none"
+                    });
+
+                } else {
+
+                    $('.main-header').css({
+                        "display": "grid"
+                    });
+
+                    $('.gofullscreen, body, html').css({
+                        "overflow-x": "hidden"
+                    });
+
+                    $('.logoOR').css({
+                        "transform": "scale(0)"
+                    });
+                    $('.main-rules, .mobile-header, .main-home').css({
+                        "transform": "translateY(-0px)"
+                    });
+                    $('.arrows-1').css({
+                        "transform": "translate(0px,-0px)"
+
+                    });
+                }
+            }
+
         $('.rules>p').each(function (i) {
             var BotOfObj = $(this).offset().top + $(this).outerHeight();
             var BotOfWin = $(window).scrollTop() + $(window).height();
