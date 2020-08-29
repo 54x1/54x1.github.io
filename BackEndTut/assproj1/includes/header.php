7a87f07f65db212b1bfc52e580b729ca0b971bb3
54x1 <five4x1@gmail.com>
2020-08-29 18:12:18 +1000

    update

--- BackEndTut/assproj1/includes/header.php
+++ BackEndTut/assproj1/includes/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title><?php echo $title; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <link rel="icon" href="https://54x1.github.io/AniBuy/img/icons/favicon.ico" type="image/x-icon" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
